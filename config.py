3899d24ff0aafe69bc6cc9de1e0958d5955d42a6
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-04 15:41:55 +0600

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,8 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://teamprosperpay:AbidAbdull
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
+ 
+
 FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002077054432"))
 FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002003740934"))
 FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002125561929"))
