a59b93e29ecca59cbcdfcca9f41eec177d7d9bb9
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-04 15:03:03 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,6 +11,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, AUTO_DELETE_TIME, AUTO_DELETE_MSG, JOIN_REQUEST_ENABLE
 from helper_func import subscribed,decode, get_messages, delete_file
 from database.database import add_user, del_user, full_userbase, present_user
+from plugins.FORCESUB import f_invitelink
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -160,12 +161,16 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 
 #=====================================================================================##
 
+def get_valid_invitelink():
+    # Return f_invitelink if set, otherwise return invitelink
+    return f_invitelink if f_invitelink else invitelink
+
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            InlineKeyboardButton(text="Join Channel", url=url=get_valid_invitelink()),
             InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
         ],
         [
